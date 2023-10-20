331cf2811ffde9a23cafa1b5ee600472364ed422
nizarabdulkholiq <kholiqnnabdul@gmail.com>
2023-10-20 14:15:29 +0800

    main

--- main.go
+++ main.go
@@ -2,18 +2,25 @@ package main
 
 import (
 	"context"
+	"errors"
 	"fmt"
+	"html/template"
 	"net/http"
 	"time"
 
 	"github.com/gorilla/mux"
-	"github.com/o1egl/paseto"
+	"github.com/o1egl/paseto/v2"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 var mongoClient *mongo.Client
+
+type User struct {
+	Username string `bson:"username"`
+	Password string `bson:"password"`
+}
 
 func main() {
 	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
@@ -26,19 +33,49 @@ func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/login", loginHandler).Methods("GET", "POST")
 	r.HandleFunc("/dashboard", dashboardHandler).Methods("GET")
-	r.HandleFunc("/logout", logoutHandler).Methods("GET")
+	r.HandleFunc("/logout", logoutHandler).Methods("POST")
 
 	http.Handle("/", r)
 	http.ListenAndServe(":8080", nil)
 }
 
-type User struct {
-	Username string `bson:"username"`
-	Password string `bson:"password"`
-}
-
 func loginHandler(w http.ResponseWriter, r *http.Request) {
-	// ...
+	if r.Method == http.MethodPost {
+		user, err := findUser(r.FormValue("username"), r.FormValue("password"))
+		if err != nil {
+			http.Error(w, "Invalid login credentials", http.StatusUnauthorized)
+			return
+		}
+
+		token, err := generateToken(user.Username)
+		if err != nil {
+			http.Error(w, "Failed to generate token", http.StatusInternalServerError)
+			return
+		}
+
+		expires := time.Now().Add(24 * time.Hour)
+		cookie := http.Cookie{
+			Name:     "token",
+			Value:    token,
+			Expires:  expires,
+			Secure:   true,
+			HttpOnly: true,
+			SameSite: http.SameSiteStrictMode,
+			Path:     "/",
+		}
+		http.SetCookie(w, &cookie)
+
+		http.Redirect(w, r, "/dashboard", http.StatusFound)
+		return
+	}
+
+	cookie, err := r.Cookie("token")
+	if err == nil && cookie.Value != "" {
+		http.Redirect(w, r, "/dashboard", http.StatusFound)
+		return
+	}
+
+	http.ServeFile(w, r, "login.html")
 }
 
 func dashboardHandler(w http.ResponseWriter, r *http.Request) {
@@ -48,16 +85,48 @@ func dashboardHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// Handle logic for dashboard here, check token validity, etc.
-	fmt.Fprintf(w, "Welcome to Dashboard!")
+	username, err := getUsernameFromToken(cookie.Value)
+	if err != nil {
+		http.Error(w, "Invalid token", http.StatusUnauthorized)
+		return
+	}
+
+	tmpl, err := template.ParseFiles("dashboard.html")
+	if err != nil {
+		http.Error(w, "Internal server error", http.StatusInternalServerError)
+		return
+	}
+
+	data := struct {
+		Username string
+	}{
+		Username: username,
+	}
+
+	err = tmpl.Execute(w, data)
+	if err != nil {
+		http.Error(w, "Internal server error", http.StatusInternalServerError)
+		return
+	}
 }
 
 func logoutHandler(w http.ResponseWriter, r *http.Request) {
-	// ...
+	cookie := http.Cookie{
+		Name:   "token",
+		Value:  "",
+		MaxAge: -1,
+		Secure: true,
+		HttpOnly: true,
+		SameSite: http.SameSiteStrictMode,
+		Path:   "/",
+	}
+	http.SetCookie(w, &cookie)
+
+	http.Redirect(w, r, "/login", http.StatusFound)
 }
 
 func findUser(username, password string) (*User, error) {
-	collection := mongoClient.Database("paseto").Collection("info") // Nama basis data dan koleksi diubah
+	collection := mongoClient.Database("paseto").Collection("info")
 	var user User
 	filter := bson.M{"username": username, "password": password}
 	err := collection.FindOne(context.TODO(), filter).Decode(&user)
@@ -68,24 +137,16 @@ func findUser(username, password string) (*User, error) {
 }
 
 func generateToken(username string) (string, error) {
-	// Ganti "your-secret-key" dengan kunci rahasia yang aman
 	secretKey := []byte("123456")
 
-	// Generate PASETO token
 	v2 := paseto.NewV2()
 	now := time.Now()
-	expiration := now.Add(24 * time.Hour) // Token expires in 24 hours
+	expiration := now.Add(24 * time.Hour)
 
-	// Payload adalah data yang ingin Anda sertakan dalam token (contoh: username dan waktu kedaluwarsa)
 	payload := map[string]interface{}{
 		"username": username,
-		"exp":      expiration, // Tambahkan waktu kedaluwarsa ke dalam payload
 	}
 
-	// Mengenkripsi payload ke dalam token
 	token, err := v2.Encrypt(secretKey, payload, nil)
 	if err != nil {
 		return "", err
-	}
-	return token, nil
-}
